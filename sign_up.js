d6f758ee4ffd42925530f4c77c321916c1eace92
Baya Meddah <148076771+BayaMeddahNIT@users.noreply.github.com>
2025-05-13 20:10:38 +0200

    Create sign_up.js
    code js

---
+++ sign_up.js
@@ -0,0 +1,22 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const params = new URLSearchParams(window.location.search);
+    const messageBox = document.getElementById("messageBox");
+
+    if (!messageBox) return;
+
+    if (params.has('error')) {
+        messageBox.textContent = params.get('error');
+        messageBox.classList.remove('hidden');
+        messageBox.classList.add('error');
+    } else if (params.has('success')) {
+        messageBox.textContent = params.get('success');
+        messageBox.classList.remove('hidden');
+        messageBox.classList.add('success');
+    }
+
+    if (!messageBox.classList.contains("hidden")) {
+        setTimeout(() => {
+            messageBox.style.display = "none";
+        }, 5000); 
+    }
+});
